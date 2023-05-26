ba365bc764ff3f38ebb6d6516c277909edc02597
rojastamaraa <rojastamaranya@gmail.com>
2023-05-26 17:45:07 -0300

    1.1

--- Ejer1.1/Program.cs
+++ Ejer1.1/Program.cs
@@ -42,8 +42,28 @@ namespace ejer1._1
 			}
 			return false;
 		}
+		public static Persona buscarr(List<Persona> list, int dni)
+		{
+			for (int i = 0; i < list.Count; i++)
+			{
+				if (list[i].dni == dni)
+				{
+				return list[i];
+				}
+			}
+			return null;
+		}
+		public static Persona eliminar (List<Persona> list, int dni)
+			{
+			Persona elimi = Persona.buscarr(list, dni);
+			if(elimi != null)
+			{
+				list.Remove(elimi);
+				return list[0];
+			}
+				return null;
+			} 
 
-	}
 	internal class Program
 	{
 
@@ -60,13 +80,31 @@ namespace ejer1._1
 			dni = int.Parse(Console.ReadLine());
 			bool encontro = Persona.buscar(list, dni);
 			
-			if(encontro == true)
-			{
-				Console.Write("queso");
-			}
+			Persona pers = Persona.buscarr(list, dni);
+				if(pers!=null)
+				{
+					Console.Write(pers.name);
+				}
+				else 
+				{
+					Console.Write("No se encontro");
+				}
+			
+				int dnii;
+				Console.Write("Ingrese un DNI para eliminar: ");
+				dnii = int.Parse(Console.ReadLine());
+				Persona chu = Persona.eliminar(list, dnii);
+				if(chu == null)
+				{
+					Console.Write ("No existe");
+				}
+				else 
+				{
+					Console.Write("Eliminado");
+				}
+
 
 			Console.ReadKey();
 
-		}
 	}
-}
+}}}
