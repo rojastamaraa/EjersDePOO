4ee302ee98d82764856c49672b1fede9bb4894e3
rojastamaraa <rojastamaranya@gmail.com>
2023-07-23 23:55:27 -0300

    ejer 11 y 12

---
+++ TamaraRojas/EjOb11/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjOb11
+{
+	interface Interfaz
+	{
+		void info(Random r);
+	}
+
+	class Apuesta : Interfaz
+	{
+		public int resultado;
+		public int pozo;
+
+		public Apuesta(Random r)
+		{
+			info(r);
+		}
+
+		public void info(Random r)
+		{
+			resultado = r.Next(1, 10);
+		}
+
+		public bool resultados(List<Participante> p)
+		{
+			for (int i = 0; i < p.Count; i++)
+			{
+				pozo = pozo + 1;
+			}
+
+			for (int i = 0; i < p.Count; i++)
+			{
+				if (p[i].resultado == resultado)
+				{
+					p[i].vecesGanadas = p[i].vecesGanadas + 1;
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public bool juegoFinalizado(List<Participante> p)
+		{
+			for (int i = 0; i < p.Count; i++)
+			{
+				if (p[i].vecesGanadas == 2)
+				{
+					p[i].dinero = p[i].dinero + pozo;
+					pozo = 0;
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+
+	class Participante
+	{
+		public int resultado;
+		public int dinero;
+		public int vecesGanadas;
+
+		public Participante(Random r)
+		{
+			info(r);
+		}
+
+		public void info(Random r)
+		{
+			dinero = r.Next(5,20);
+		}
+
+		public void apostar(Random r)
+		{
+			if (dinero > 0)
+			{
+				resultado = r.Next(1, 10);
+				dinero = dinero - 1;
+			}
+			else
+			{
+				//el resultado es cero entonces no participa de la apuesta B)
+				resultado = 0;
+			}
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Random r = new Random();
+			Apuesta a = new Apuesta(r);
+			List<Participante> p = new List<Participante>();
+			for (int i = 0; i < 5; i++)
+			{
+				p.Add(new Participante(r));
+			}
+
+			Console.WriteLine("Resultado: " + a.resultado + " Pozo: " + a.pozo);
+
+			bool juego = false;
+			while (juego == false)
+			{
+				ConsoleKeyInfo tecla = Console.ReadKey(true);
+				if (tecla.Key == ConsoleKey.Enter)
+				{
+					Console.Clear();
+					Console.WriteLine("Resultado: " + a.resultado + " Pozo: " + a.pozo);
+					for (int i = 0; i < p.Count; i++)
+					{
+						p[i].apostar(r);
+					}
+
+					a.resultados(p);
+					bool aa = a.juegoFinalizado(p);
+					if (aa == true)
+					{
+						Console.Clear();
+						Console.WriteLine("Resultado: " + a.resultado + " Pozo: " + a.pozo);
+						Console.ForegroundColor = ConsoleColor.Green;
+						Console.WriteLine("¡¡¡Hubo un ganador!!!");
+						for (int i = 0; i < p.Count; i++)
+						{
+							Console.ForegroundColor = ConsoleColor.White;
+							Console.WriteLine("Jugador: " + i + " Resultado: " + p[i].resultado + " Dinero: " + p[i].dinero + " Veces ganadas: " + p[i].vecesGanadas);
+						}
+						juego = true;
+					}
+					else
+					{
+						Console.Clear();
+						Console.WriteLine("Resultado: " + a.resultado + " Pozo: " + a.pozo);
+						Console.WriteLine("Aun no hay ganadores");
+						for (int i = 0; i < p.Count; i++)
+						{
+							if (p[i].dinero > 0)
+							{
+								Console.WriteLine("Jugador: " + i + " Resultado: " + p[i].resultado + " Dinero: " + p[i].dinero + " Veces ganadas: " + p[i].vecesGanadas);
+							}
+							else
+							{
+								Console.WriteLine("Jugador: " + i + " ya no participa debido a que se quedo sin dinero");
+							}
+						}
+					}
+					a.info(r);
+				}
+			}
+
+			Console.Read();
+		}
+	}
+}
---
+++ TamaraRojas/EjOb12/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace EjOb12
+{
+	class Revolver
+	{
+		public int posicionActual;
+		public int posicionBala;
+
+		public Revolver(Random r)
+		{
+			posicionActual = r.Next(1,6);
+			posicionBala = r.Next(1,6);
+		}
+
+		public bool disparar()
+		{
+			siguienteBala();
+			if (posicionActual == posicionBala)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		public void siguienteBala()
+		{
+			if (posicionActual + 1 > 6)
+			{
+				posicionActual = 1;
+			}
+			else
+			{
+				posicionActual = posicionActual + 1;
+			}
+		}
+	}
+
+	class Jugador
+	{
+		public int ID;
+		public string nombre;
+		public bool vivo;
+
+		public Jugador(int ID, string nombre)
+		{
+			this.ID = ID;
+			this.nombre = nombre;
+			vivo = true;
+		}
+
+		public void disparar(Revolver r)
+		{
+			bool d = r.disparar();
+			if (d == true)
+			{
+				vivo = false;
+			}
+		}
+	}
+
+	class Jugadores
+	{
+		public List<Jugador> j;
+		public Revolver r;
+
+		public Jugadores(List<Jugador> j, Revolver r)
+		{
+			this.j = j;
+			this.r = r;
+		}
+
+		public bool finJuego(Jugador j)
+		{
+			if (j.vivo == false)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		public bool ronda(Jugador j, Revolver r)
+		{
+			j.disparar(r);
+			return finJuego(j);
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Random rr = new Random();
+			List<Jugador> j = new List<Jugador>();
+			Revolver r = new Revolver(rr);
+
+			Console.CursorVisible = false;
+
+			Console.SetCursorPosition(45, 13);
+			Console.Write("Ingrese cantidad de jugadores: ");
+			int cant = int.Parse(Console.ReadLine());
+
+			if (cant < 1 || cant > 6)
+			{
+				cant = 6;
+			}
+
+			for (int i = 0; i < cant; i++)
+			{
+				j.Add(new Jugador(i, "Jugador" + (i + 1)));
+			}
+
+			Jugadores js = new Jugadores(j, r);
+
+			Console.Clear();
+			Console.SetCursorPosition(45, 13);
+			Console.Write("Presione Enter para disparar");
+			
+			bool fin = false;
+			int jugadorActual = 0;
+
+			while (fin == false)
+			{
+
+				ConsoleKeyInfo tecla = Console.ReadKey(true);
+
+				Console.Clear();
+
+				if (tecla.Key == ConsoleKey.Enter)
+				{
+					Console.Clear();
+
+					bool estado = js.ronda(j[jugadorActual], r);
+					//Console.WriteLine("Posicion actual: " + r.posicionActual + " Posicion de la bala: " + r.posicionBala);
+
+					if (estado == true)
+					{
+						for (int i = 0; i < 2; i++)
+						{
+							Console.Clear();
+							Console.BackgroundColor = ConsoleColor.Red;
+							Console.Clear();
+							Console.BackgroundColor = ConsoleColor.White;
+							Console.Clear();
+							Console.BackgroundColor = ConsoleColor.Yellow;						
+						}
+	
+						Console.BackgroundColor = ConsoleColor.Black;
+						Console.Clear();
+						Console.SetCursorPosition(45, 13);
+						Console.ForegroundColor = ConsoleColor.Red;
+						string mensaje = "El " + j[jugadorActual].nombre + " se disparo y murio";
+						for (int i = 0; i < mensaje.Length; i++)
+						{
+							Console.SetCursorPosition(45 + i, 13);
+							Console.Write(mensaje[i]);
+							Thread.Sleep(60);
+						}
+
+						for (int i = 0; i < 28; i++)
+						{
+							Console.BackgroundColor = ConsoleColor.Red;
+							Console.SetCursorPosition(0, 0 + i);
+							Console.WriteLine("                                                                                                                        ");
+							Thread.Sleep(100);
+						}
+
+						Console.SetCursorPosition(55, 13);
+						Console.ForegroundColor = ConsoleColor.Black;
+						Console.WriteLine("Fin del juego");
+
+						fin = true;
+					}
+					else
+					{
+						Console.SetCursorPosition(43, 13);
+						Console.WriteLine("El " + j[jugadorActual].nombre + " se disparo y no habia bala");
+					}
+
+					if (jugadorActual + 1 >= cant)
+					{
+						jugadorActual = 0;
+					}
+					else
+					{
+						jugadorActual = jugadorActual + 1;
+					}
+				}
+			}
+			Console.Read();
+		}
+	}
+}
