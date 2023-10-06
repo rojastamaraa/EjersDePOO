d8692de7bdffb20bef58bd0a9fdd31b427c13b60
matoe69 <santiagomaytahuanca@gmail.com>
2023-10-06 15:02:12 -0300

    a

---
+++ Evaluacion/Evaluacion/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Evaluacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.Ordenar = new System.Windows.Forms.Button();
+            this.Eliminar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(162, 212);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(626, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(162, 212);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // Ordenar
+            // 
+            this.Ordenar.Location = new System.Drawing.Point(329, 88);
+            this.Ordenar.Name = "Ordenar";
+            this.Ordenar.Size = new System.Drawing.Size(75, 23);
+            this.Ordenar.TabIndex = 2;
+            this.Ordenar.Text = "Ordenar";
+            this.Ordenar.UseVisualStyleBackColor = true;
+            this.Ordenar.Click += new System.EventHandler(this.Ordenar_Click);
+            // 
+            // Eliminar
+            // 
+            this.Eliminar.Location = new System.Drawing.Point(416, 88);
+            this.Eliminar.Name = "Eliminar";
+            this.Eliminar.Size = new System.Drawing.Size(75, 23);
+            this.Eliminar.TabIndex = 3;
+            this.Eliminar.Text = "Eliminar";
+            this.Eliminar.UseVisualStyleBackColor = true;
+            this.Eliminar.Click += new System.EventHandler(this.Eliminar_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Intercambiar 5 < x";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(329, 146);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Intercambiar 1er y Ult";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Eliminar);
+            this.Controls.Add(this.Ordenar);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button Ordenar;
+        private System.Windows.Forms.Button Eliminar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Evaluacion/Evaluacion/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Evaluacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string[] n = { "tamara", "chanculio", "mortadela", "renata", "michael jackson", "ñ", "siete", "c#", "linea", "intro" };
+            string[] n2 = { "rojas", "chanjunio", "caballo", "serenata", "mercury", "n", "ziete", "c#sharp", "liea", "io" };
+            for (int i = 0; i < 10; i++)
+            {
+                listBox1.Items.Add(n[i]);
+                listBox2.Items.Add(n2[i]);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Eliminar_Click(object sender, EventArgs e)
+        {
+            string n, n2;
+            for (int i = 0;i < listBox1.Items.Count; i++)
+            {
+                n = listBox1.Items[i].ToString();
+                
+                if (n.Length <= 5)
+                {
+                    listBox1.Items.RemoveAt(i);
+
+                    i = i - 1;
+                }   
+            }
+            for (int i = 0; i < listBox2.Items.Count; i++)
+            {
+                n2 = listBox2.Items[i].ToString();
+                if (n2.Length <= 5)
+                {
+                    listBox2.Items.RemoveAt(i);
+
+                    i--;
+                }
+            }
+        }
+
+        private void Ordenar_Click(object sender, EventArgs e)
+        {
+            listBox1.Sorted = true;
+            listBox2.Sorted = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string n, n2;
+            List<string> list = new List<string>();
+            List<string> list2 = new List<string>();
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                n = listBox1.Items[i].ToString();
+
+                if (n.Length <= 5)
+                {
+                    list.Add(n);
+                }
+            }
+            for (int i = 0; i < listBox2.Items.Count; i++)
+            {
+                n2 = listBox2.Items[i].ToString();
+                if (n2.Length <= 5)
+                {
+                    list2.Add(n2);
+                }
+            }
+
+            for(int i = 0;i < list.Count;i++) 
+            {
+                listBox2.Items.Add(list[i]);
+            }
+
+            for (int i = 0; i < list2.Count; i++)
+            {
+                listBox1.Items.Add(list2[i]);
+            }
+
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                for (int j = 0; j < list.Count; ++j)
+                if (listBox1.Items[i].ToString() == list[j])
+                {
+                    listBox1.Items.RemoveAt(i);
+                }
+            }
+
+            for (int i = 0; i < listBox2.Items.Count; i++)
+            {
+                for (int j = 0; j < list2.Count; ++j)
+                    if (listBox2.Items[i].ToString() == list2[j])
+                    {
+                        listBox2.Items.RemoveAt(i);
+                    }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //tarde mucho y no alcance ñe
+            int aux = listBox1.Items.Count;
+            string pr = listBox1.Items[0].ToString();
+            string ult = listBox1.Items[aux-1].ToString();
+
+            int aux2 = listBox2.Items.Count;
+            string pr2 = listBox2.Items[0].ToString();
+            string ult2 = listBox2.Items[aux2 - 1].ToString();
+
+            listBox1.Text.Insert(0, "");
+        }
+    }
+}
+ 
