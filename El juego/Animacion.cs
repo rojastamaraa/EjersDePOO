d6e874d73a1c33fb2b2bd4bfbd8eb7235170784b
rojastamaraa <rojastamaranya@gmail.com>
2023-11-08 19:03:21 -0300

    chica free love

---
+++ El juego/Animacion.cs
@@ -0,0 +1,49 @@
+﻿using El_juego;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+public class Animacion
+{
+	public Texture2D textura;
+	public Vector2 pos;
+	public SpriteEffects spriteEffect;
+	public float temporizador, tiempo;
+	public int ancho, alto, X, Y, cantFrames, frameActual;
+	public Animacion(Texture2D textura, Vector2 pos, SpriteEffects spriteEffect, float tiempo, int ancho, int alto, int X, int Y, int cantFrames)
+	{
+		this.textura = textura;
+		this.pos = pos;
+		this.spriteEffect = spriteEffect;
+		this.tiempo = tiempo;
+		this.ancho = ancho;
+		this.alto = alto;
+		this.X = X;
+		this.Y = Y;
+		this.cantFrames = cantFrames;
+	}
+
+	public void Update(GameTime gameTime)
+	{
+		temporizador += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+		if (temporizador > tiempo)
+		{
+			if (frameActual < cantFrames - 1)
+			{
+				frameActual++;
+				temporizador = 0;
+			}
+			else 
+			{
+				frameActual = 0;
+				temporizador = 0;
+			}
+		}
+	}
+
+	public void Draw(SpriteBatch spriteBatch)
+	{
+		Rectangle rectangle = new Rectangle((frameActual*ancho), Y*alto, ancho, alto);
+		spriteBatch.Draw(textura, pos, rectangle, Color.White, 0f, Vector2.Zero, 0.1f, spriteEffect, 0f);
+	}
+}
+
--- El juego/Game1.cs
+++ El juego/Game1.cs
@@ -11,13 +11,11 @@ namespace El_juego
 	{
 		private GraphicsDeviceManager _graphics;
 		private SpriteBatch _spriteBatch;
-
-		private Texture2D tbh;
+		public Animacion tbh;
+		public Texture2D tbhTexture;
 		private Vector2 tbhPos = new Vector2(100, 100);
+		public int indiceTbh;
 		private SpriteEffects spriteEffect;
-		Rectangle[] sourceRectangles;
-		int c = 0;
-		int pix = 0;
 
 		public Game1()
 		{
@@ -36,25 +34,10 @@ namespace El_juego
 		protected override void LoadContent()
 		{
 			_spriteBatch = new SpriteBatch(GraphicsDevice);
-			tbh = Content.Load<Texture2D>("img/tbhwalk"); 
+			tbhTexture = Content.Load<Texture2D>("img/tbhwalk");
 			spriteEffect = SpriteEffects.None;
-			// TODO: use this.Content to load your game content here
+			tbh = new Animacion(tbhTexture, tbhPos, spriteEffect, 200, 609, 609, 0, indiceTbh, 4);
 
-			sourceRectangles = new Rectangle[12];
-			sourceRectangles[0] = new Rectangle(0, 0, 609, 609);
-			sourceRectangles[1] = new Rectangle(609, 0, 609, 609);
-			sourceRectangles[2] = new Rectangle(1218, 0, 609, 609);
-			sourceRectangles[3] = new Rectangle(1827, 0, 609, 609);
-
-			sourceRectangles[4] = new Rectangle(0, 609, 609, 609);
-			sourceRectangles[5] = new Rectangle(609, 609, 609, 609);
-			sourceRectangles[6] = new Rectangle(1218, 609, 609, 609);
-			sourceRectangles[7] = new Rectangle(1827, 609, 609, 609);
-
-			sourceRectangles[8] = new Rectangle(0, 1218, 609, 609);
-			sourceRectangles[9] = new Rectangle(609, 1218, 609, 609);
-			sourceRectangles[10] = new Rectangle(1218, 1218, 609, 609);
-			sourceRectangles[11] = new Rectangle(1827, 1218, 609, 609);
 		}
 
 		protected override void Update(GameTime gameTime)
@@ -62,108 +45,37 @@ namespace El_juego
 			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
 				Exit();
 
-			// TODO: Add your update logic here
-			
 			KeyboardState key = Keyboard.GetState();
-
 			if (key.GetPressedKeys().Length == 0)
 			{
-				if (c <= 3)
-				{
-					c = 0;
-				}
-				else if (c > 3 && c <=7)
-				{
-					c = 4;
-				}
-				else
-				{
-					c = 8;
-				}
+				tbh.frameActual = 0;
 			}
-
-			if (key.IsKeyDown(Keys.Down))
+				if (key.IsKeyDown(Keys.Down))
 			{
-				tbhPos.Y += 2;
-				pix += 1;
-				if (c > 3)
-				{
-					c = 0;
-				}
-				if (c < 3 && pix > 5)
-				{
-					c = c + 1;
-					pix = 0;
-				}
-				else if (c == 3 && pix > 5)
-				{
-					c = 0;
-					pix = 0;
-				}
+				indiceTbh = 0;
+				tbh.Update(gameTime);
+				tbh.pos.Y += 1;
 			}
 			if (key.IsKeyDown(Keys.Up))
 			{
-				tbhPos.Y -= 2;
-				pix += 1;
-				if (c > 7 || c < 4)
-				{
-					c = 4;
-				}
-				if (c < 7 && pix > 5)
-				{
-					c = c + 1;
-					pix = 0;
-				}
-				else if (c == 7 && pix > 5)
-				{
-					c = 4;
-					pix = 0;
-				}
-
+				indiceTbh = 1;
+				tbh.Update(gameTime);
+				tbh.pos.Y -= 1;
 			}
 			if (key.IsKeyDown(Keys.Left))
 			{
 				spriteEffect = SpriteEffects.None;
-				tbhPos.X -= 2;
-				pix += 1;
-				if (c < 8)
-				{
-					c = 8;
-				}
-				if (c < 11 && pix > 5)
-				{
-					c = c + 1;
-					pix = 0;
-				}
-				else if (c == 11 && pix > 5)
-				{
-					c = 8;
-					pix = 0;
-				}
-
+				indiceTbh = 2;
+				tbh.Update(gameTime);
+				tbh.pos.X -= 1;
 			}
 			if (key.IsKeyDown(Keys.Right))
 			{
 				spriteEffect = SpriteEffects.FlipHorizontally;
-				tbhPos.X += 2;
-				pix += 1;
-				if (c < 8)
-				{
-					c = 8;
-				}
-				if (c < 11 && pix > 5)
-				{
-					c = c + 1;
-					pix = 0;
-				}
-				else if (c == 11 && pix > 5)
-				{
-					c = 8;
-					pix = 0;
-				}
-
+				indiceTbh = 2;
+				tbh.Update(gameTime);
+				tbh.pos.X += 1;
 			}
-
 
 			base.Update(gameTime);
 		}
@@ -172,10 +84,11 @@ namespace El_juego
 		{
 			GraphicsDevice.Clear(Color.CornflowerBlue);
 
-			// TODO: Add your drawing code here
 			_spriteBatch.Begin();
 
-			_spriteBatch.Draw(tbh, tbhPos, sourceRectangles[c], Color.White, 0f, Vector2.Zero, 0.1f, spriteEffect, 0f);
+			tbh.spriteEffect= spriteEffect;
+			tbh.Y = indiceTbh;
+			tbh.Draw(_spriteBatch);
 
 			_spriteBatch.End();
 			base.Draw(gameTime);
